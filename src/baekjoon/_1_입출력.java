56bcc579f2701b521f077c8c320afbce6d2f6e15
GyeoLe Lee <41763240+LEEGYEOLE@users.noreply.github.com>
2023-05-15 21:15:30 +0900

    백준 알고리즘 입출력과 사칙연산 1, 10869

---
+++ src/baekjoon/_1_입출력.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class _1_입출력 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt()*sc.nextInt());
+		sc.close();
+	}
+}
