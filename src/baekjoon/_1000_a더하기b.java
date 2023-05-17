70430364dc261387edcc6b400c73540c539b445f
GyeoLe Lee <41763240+LEEGYEOLE@users.noreply.github.com>
2023-05-17 11:57:58 +0900

    백준 알고리즘 기본 알고리즘 1000

---
+++ src/baekjoon/_1000_a더하기b.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class _1000_a더하기b {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt()+sc.nextInt());
+		sc.close();
+	}
+}
