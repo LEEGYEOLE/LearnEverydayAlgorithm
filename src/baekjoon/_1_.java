361493624618777fd81d88d72b505beecf8a0e04
GyeoLe Lee <41763240+LEEGYEOLE@users.noreply.github.com>
2023-05-17 11:58:33 +0900

    백준 알고리즘 기본 알고리즘 입출력 기본 class

--- src/baekjoon/_1_.java
+++ src/baekjoon/_1_.java
@@ -2,7 +2,7 @@ package baekjoon;
 
 import java.util.Scanner;
 
-public class _1_입출력 {
+public class _1_ {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println(sc.nextInt()*sc.nextInt());
