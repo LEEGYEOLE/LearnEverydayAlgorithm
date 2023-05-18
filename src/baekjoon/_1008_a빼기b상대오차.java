fadf5a3499d2502a5b3583a984c2177b96d32785
GyeoLe Lee <41763240+LEEGYEOLE@users.noreply.github.com>
2023-05-18 12:49:18 +0900

    백준 알고리즘 기본 1008

---
+++ src/baekjoon/_1008_a빼기b상대오차.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class _1008_a빼기b상대오차 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		System.out.println(a/b);
+		sc.close();
+	}
+}
